f4c94ea6d55cf4e22311c744f9257b1d50d4d0c9
seok-jong <sjhyeon1222@gmail.com>
2021-01-27 13:50:19 +0900

    coma

--- 3주차/1.Palindrome_Linked_List.py
+++ 3주차/1.Palindrome_Linked_List.py
@@ -21,3 +21,21 @@ def isPalindrome(self, head: ListNode) -> bool:
                 continue
         return True
 
+
+def isPalindrome(self, head: ListNode) -> bool:
+        q:List=[]
+
+        if not head:
+            return True
+
+        node=head
+
+        while node is not None:
+            q.append(node.val)
+            node=node.next
+
+        while len(q)>1:
+            if q.pop(0)!=q.pop():
+                return False
+
+        return True
